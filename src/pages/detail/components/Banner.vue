78b6b60c4aa499fa86c1c366dac0d2ef8677a723
chenlan <chenlan@chenlandeMacBook-Pro.local>
2018-10-19 17:09:53 +0800

    banner finish

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,68 @@
+<template>
+<div>
+    <div class="banner" @click="handleBannerClick">
+       <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1507/36/ce3d2d6c9ab44d67ae68d940b8781829.water.jpg_600x330_f68eb231.jpg"/>
+       <div class="banner-info">
+         <div class="banner-title">北京野生动物园(AAAA景区)</div>
+         <div class="banner-number"><span class="iconfont banner-icon">&#xe632;</span>39</div>
+       </div>
+    </div>
+    <common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+</div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGallary: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/1501/6e/d0137416c0a91e63b63e39679745a0b2.water.jpg_r_800x800_bfbb92b8.jpg']
+    }
+  },
+  components: {
+    CommonGallary
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position: relative
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      line-height: .6rem
+      color: #fff
+      background-image: linear-gradient(top,rgba(0, 0, 0, 0),rgba(0, 0, 0, 0.8))
+      .banner-title
+        flex: 1
+        font-size: .32rem
+        padding: 0 .2rem
+      .banner-number
+        padding: 0 .4rem
+        height: .32rem
+        line-height: .32rem
+        margin-top: .14rem
+        border-radius: .2rem
+        background: rgba(0, 0, 0, .8)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
+</style>
